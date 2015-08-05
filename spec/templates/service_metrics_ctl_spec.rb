faa848d47612673cb6ea07428a620856f06ee48f
Alex Ley <aley@pivotal.io>
2015-08-05 09:32:12 +0100

    Dont really need to test default as we would then be testing bosh
    [#93353838]
    
    Signed-off-by: Jamie Monserrate <jmonserrate@pivotal.io>

--- spec/templates/service_metrics_ctl_spec.rb
+++ spec/templates/service_metrics_ctl_spec.rb
@@ -5,40 +5,17 @@ require 'yaml'
 describe 'Service Metrics Ctl script' do
   let(:manifest){ YAML.load_file('manifests/service-metrics-lite.yml')}
 
-  describe "metrics-interval" do
-    context "when no value provided in deployment manifest" do
+  before do
+    manifest['properties']['service_metrics']['debug'] = false
+    manifest['properties']['metron_agent']['dropsonde_incoming_port'] = 12345
+  end
 
-      before do
-        manifest['properties']['service_metrics']['debug'] = false
-        manifest['properties']['service_metrics']['interval'] = nil
-      end
+  it "templates the value of the interval" do
+    manifest['properties']['service_metrics']['interval'] = 5
 
-      #TODO combine the manifest / spec to pass all properties to the template
-      xit "defaults to 60 seconds" do
-        renderer = Bosh::Template::Renderer.new({context: manifest.to_json})
+    renderer = Bosh::Template::Renderer.new({context: manifest.to_json})
+    rendered_template = renderer.render('jobs/service-metrics/templates/service_metrics_ctl.erb')
 
-        rendered_template = renderer.render('jobs/service-metrics/templates/service_metrics_ctl.erb')
-
-        expect(rendered_template).to include("--metrics-interval 60")
-      end
-    end
-
-    context "when a value is provided in the deployment manifest" do
-
-      before do
-        manifest['properties']['service_metrics']['debug'] = false
-        manifest['properties']['service_metrics']['interval'] = 5
-        manifest['properties']['metron_agent']['dropsonde_incoming_port'] = 12345
-      end
-
-      it "uses the value in the argument to the binary" do
-        renderer = Bosh::Template::Renderer.new({context: manifest.to_json})
-
-        rendered_template = renderer.render('jobs/service-metrics/templates/service_metrics_ctl.erb')
-
-        expect(rendered_template).to include("--metrics-interval 5")
-      end
-
-    end
+    expect(rendered_template).to include("--metrics-interval 5")
   end
 end
