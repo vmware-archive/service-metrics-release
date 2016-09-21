1e97abf64708e942803f62cf172d0ad2dfa9c483
Alex Blease <ablease@pivotal.io>
2016-09-21 15:24:15 +0100

    Add ci tasks to tag service-metrics and docs
    Signed-off-by: Josh Hill <jhill@pivotal.io>

---
+++ ci/scripts/push-metrics-tag.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -eu
+set -o pipefail
+
+RELEASE_DIR=$(pwd)/metrics-final-tag
+METRICS_DIR=$RELEASE_DIR/src/service-metrics
+TAG_DIR=$(pwd)/metrics-with-tag
+
+RELEASE_TAG="$(git -C "$RELEASE_DIR" tag --list 'v*' --contains HEAD)"
+git -C "$METRICS_DIR" tag "$RELEASE_TAG"
+git clone "$METRICS_DIR" "$TAG_DIR"
