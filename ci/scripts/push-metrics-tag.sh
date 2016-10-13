b22b9801cad7f4a499062972109d6b579c79908e
Sam Gunaratne <rgunaratne@pivotal.io>
2016-10-13 12:26:45 +0100

    Fix push tag ci scripts
    - Make them executable
    - Change path to service metrics code
    
    [#132230215]
    
    Signed-off-by: Josh Hill <jhill@pivotal.io>

--- ci/scripts/push-metrics-tag.sh
+++ ci/scripts/push-metrics-tag.sh
@@ -2,7 +2,7 @@
 set -o pipefail
 
 RELEASE_DIR=$(pwd)/metrics-final-tag
-METRICS_DIR=$RELEASE_DIR/src/service-metrics
+METRICS_DIR=$RELEASE_DIR/src/github.com/pivotal-cf-experimental/service-metrics
 TAG_DIR=$(pwd)/metrics-with-tag
 
 RELEASE_TAG="$(git -C "$RELEASE_DIR" tag --list 'v*' --contains HEAD)"
